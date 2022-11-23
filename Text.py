813ae5be0ec73bd123e933c3a4f3c4e8ab9eba9a
Qinyue <qinyue.liu@etu.unistra.fr>
2022-11-23 15:32:20 +0100

    conllu added

--- Text.py
+++ Text.py
@@ -24,37 +24,37 @@ class Text:
             
         return toks
     
-    def POS_tagging(self):
+    def POS_tagging(self, terminal_output):
         pos_dic = {}
         print("\nPOS tagging results:\n")
         for tok in self.nlp_toks:
-            print("{} ---> {}\n".format(tok.text, tok.pos_))
+            if (terminal_output):
+                print("{} ---> {}\n".format(tok.text, tok.pos_))
             if (tok.text not in pos_dic):
                 pos_dic[tok.text] = [tok.pos_]
             else:
                 if (pos_dic[tok.text][0] != tok.pos_): # Si il y a different POS pour un meme mot:
                     pos_dic[tok.text].append(tok.pos_)
         return pos_dic
-
-
-        
     
-    def statistics(self):
+    def statistics(self, terminal_output):
         stat_dic = {}
         for tok in self.nlp_toks:
             if (tok.text not in stat_dic):
                 stat_dic[tok.text] = 1
             else:
                 stat_dic[tok.text] += 1
-        print(stat_dic)
+        if (terminal_output):
+            print(stat_dic)
         return stat_dic
     
-    def lemma(self):
+    def lemma(self, terminal_output):
         lemma_dic = {}
         print("\nLemma:\n")
         for tok in self.nlp_toks:
             lemma_dic[tok.text] = tok.lemma_
-            print("{} ---> {}\n".format(tok.text, tok.lemma_))
+            if (terminal_output):
+                print("{} ---> {}\n".format(tok.text, tok.lemma_))
         return lemma_dic
         
     def research(self, tok_cherche):
@@ -70,3 +70,43 @@ class Text:
         else:
             print("ERROR: No input text.\n")
             return False
+    
+    def to_csv(self, analyse_type, dict, path):
+        if (analyse_type == "pos"):
+            header = ["token", "POS"]
+        elif (analyse_type == "statistics"):
+            header = ["token", "Frequence"]
+        elif (analyse_type == "lemma"):
+            header = ["token", "lemma"]
+        else:
+            print("ERROR: No analyse type\n")
+            exit()
+        with open (path, "w", encoding="utf-8") as f_out:
+            f_out.write("{}\t{}\n".format(header[0], header[1]))
+            for key in dict:
+                f_out.write("{}\t{}\n".format(key, dict[key]))
+    
+    def merge_dict(self, li_dicts):
+        all_info = {}
+        for dic in li_dicts:
+            for key in dic:
+                if key in all_info:
+                    all_info[key].append(dic[key])
+                else:
+                    all_info[key] = [ dic[key] ]
+        return all_info
+
+    def to_conllu(self, dict, path):
+        count = 1
+        with open (path, "w", encoding="utf-8") as f_out:
+            for key in dict:
+                info = ""
+                for elem in dict[key]:
+                    if (isinstance(elem, str)):
+                        info += elem + "\t"
+                    elif (isinstance(elem, list)):
+                        info += elem[0] + "\t"
+                    elif (isinstance(elem, int)):
+                        info += str(elem) + "\t"
+                f_out.write("{}\t{}\t{}\n".format(str(count), key, info))
+                count += 1
--- main.py
+++ main.py
@@ -8,25 +8,13 @@ parser.add_argument("--text", help = 'texte pure pour analyser')
 parser.add_argument("--analyse_type", help = 'type pour analyser')
 parser.add_argument("--research_token", help = 'token to be searched in text')
 parser.add_argument("--csv_path", help = 'chemin pour sauvegarder fichier csv')
+parser.add_argument("--conllu_path", help = 'chemin pour sauvegarder fichier conllu')
 parser.add_argument("--language", help = 'langue (fr ou en)')
 parser.add_argument("--min_len", help = 'longeur minimal pour corpus')
 parser.add_argument("--max_len", help = 'longeur maximal pour corpus')
+parser.add_argument("--terminal_output", help = 'true or false to show or hide terminal output')
 
 
-def to_csv(analyse_type, dict, path):
-    if (analyse_type == "pos"):
-        header = ["token", "POS"]
-    elif (analyse_type == "statistics"):
-        header = ["token", "Frequence"]
-    elif (analyse_type == "lemma"):
-        header = ["token", "lemma"]
-    else:
-        print("ERROR: No analyse type\n")
-        exit()
-    with open (path, "w", encoding="utf-8") as f_out:
-        f_out.write("{},{}\n".format(header[0], header[1]))
-        for key in dict:
-            f_out.write("{},{}\n".format(key, dict[key]))
 
 def merge_text(text, txt_path):
     if (text == None):
@@ -37,21 +25,30 @@ def merge_text(text, txt_path):
                 text += line
     return text
 
-def main(text, txt_path, analyse_type, research_token, csv_path, language, min_len, max_len):
+def main(text, txt_path, analyse_type, research_token, csv_path, conllu_path, language, min_len, max_len, terminal_output):
     text = merge_text(text, txt_path)
     t = Text(text, language, min_len, max_len)
+    if (terminal_output == None):
+        terminal_output = True
     if (csv_path != None):
         print("\nLe resultat d'analyse sera sauvegardé dans le fichier: " + csv_path + "")
 
         if (analyse_type == "pos"):
-            pos_dict = t.POS_tagging()
-            to_csv(analyse_type, pos_dict, csv_path)
+            pos_dict = t.POS_tagging(terminal_output)
+            t.to_csv(analyse_type, pos_dict, csv_path)
         elif (analyse_type == "statistics"):
-            stat_dict = t.statistics()
-            to_csv(analyse_type, stat_dict, csv_path)
+            stat_dict = t.statistics(terminal_output)
+            t.to_csv(analyse_type, stat_dict, csv_path)
         elif (analyse_type == "lemma"):
-            lemma_dict = t.lemma()
-            to_csv(analyse_type, lemma_dict, csv_path)
+            lemma_dict = t.lemma(terminal_output)
+            t.to_csv(analyse_type, lemma_dict, csv_path)
+    elif (conllu_path != None):
+        print("\nLe resultat d'analyse sera sauvegardé dans le fichier: " + conllu_path + "")
+        dic_pos = t.POS_tagging(terminal_output = False)
+        dic_statistics = t.statistics(terminal_output = False)
+        dic_lemma = t.lemma(terminal_output = False)
+        all_info = t.merge_dict([dic_pos, dic_statistics, dic_lemma])
+        t.to_conllu(all_info, conllu_path)
     else:
         print("\nLe resultat d'analyse ne sera pas sauvegard.")
 
@@ -70,6 +67,7 @@ if __name__ == "__main__":
     text = args.text
     txt_path = args.txt_path
     csv_path = args.csv_path
+    conllu_path = args.conllu_path
     analyse_type = args.analyse_type
     research_token = args.research_token
     
@@ -77,4 +75,6 @@ if __name__ == "__main__":
     min_len = 1 if args.min_len == None else int(args.min_len)
     max_len = 1000 if args.max_len == None else int(args.max_len)
 
-    main(text, txt_path, analyse_type, research_token, csv_path, language, min_len, max_len)
+    terminal_output = args.terminal_output
+
+    main(text, txt_path, analyse_type, research_token, csv_path, conllu_path, language, min_len, max_len, terminal_output)
